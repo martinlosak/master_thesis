edb8f85b3d6094ecc05869cdfedb201b3e581269
martinlosak <martinlosak2@gmail.com>
2015-12-09 12:39:18 +0100

    news

--- script.R
+++ script.R
@@ -31,25 +31,51 @@ processingWeather("bratislava/weather.csv")
 mergeFunc <- function(ene_file, wea_file) {
   energy <- read.csv(ene_file)
   weather <- read.csv(wea_file)
-  merged <- merge(energy,weather,c("DateUTC","TimeUTC"),sort = FALSE)
+  merged <-
+    merge(energy,weather,c("DateUTC","TimeUTC"),sort = FALSE)
   write.csv(
     x = merged,file = "bratislava/merged.csv", quote = FALSE, row.names = FALSE
   )
 }
 mergeFunc("bratislava/energy2.csv", "bratislava/weather2.csv")
 
-drawGraph <- function(file){
+decomposeDemand <- function(file) {
   merged <- read.csv(file)
-#   library(corrplot)
-#   m <- cor(merged[,2:6])
-#   corrplot(m, method = "circle")
-  
-  # pairs(merged, panel = panel.smooth)
-#   plot(merged$Temperature_F, merged$SUM_of_MNOZSTVO, pch = 1, col = "red", cex = 0.5,
-#        xlab = "Temperature [°F]", ylab = "Consumption [MWh]")
+
   ts <- ts(merged$SUM_of_MNOZSTVO, frequency = 96, start = 0)
-  stl.dekom <- stl(ts, s.window="periodic", robust = TRUE)
-  plot(stl.dekom)
+  decom <- stl(ts, s.window = "periodic", robust = TRUE)
+  
+  ts_temp = data.frame(merged$DateUTC,merged$Temperature_F,decom$time.series[,"trend"])
+  
+  write.csv(x = ts_temp, file = "bratislava/ts_temp.csv", quote = FALSE, row.names = FALSE)
   
 }
-drawGraph("bratislava/merged.csv")
+decomposeDemand("bratislava/merged.csv")
+
+drawGraph <- function(file) {
+  ts_temp <- read.csv(file)
+  #   library(corrplot)
+  #   m <- cor(merged[,2:6])
+  #   corrplot(m, method = "circle")
+  
+  #   pairs(merged, panel = panel.smooth)
+  #   plot(merged$Temperature_F, merged$SUM_of_MNOZSTVO, pch = 1, col = "red", cex = 0.5,
+  #        xlab = "Temperature [°F]", ylab = "Consumption [MWh]")
+  #   plot(x = ts_temp$DateUTC, y = ts_temp$decom)
+  #   lines(x = ts_temp$DateUTC, y = ts_temp$Temperature_F)
+
+  x <- ts_temp$DateUTC
+  y1 <- ts_temp$decom
+  y2 <- ts_temp$Temperature_F
+  
+  
+  library(ggplot2)
+  temp_plot = ggplot(ts_temp, aes(x = DateUTC, y = Temperature_F)) + geom_line(colour = "red") + xlab("Čas") + ylab("Teplota") + theme_classic()
+  temp_plot
+#   demd_plot = ggplot(ts_temp, aes(x = DateUTC, y = decom)) + geom_line(colour = "blue") + xlab("Čas") + ylab("Spotreba") + theme_classic()
+#   demd_plot
+
+#   plot(x, y1, type = "l", col = "blue", xlab = "Čas", ylab = "Spotreba")
+#   plot(x, y2, type = "l", col = "red", xlab = "Čas", ylab = "Teplota")
+}
+drawGraph("bratislava/ts_temp.csv")
